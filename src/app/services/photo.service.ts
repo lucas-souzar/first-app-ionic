89bcc1b8ae87c578d06b47db6b8f56c05636fc47
Lucas Souza <lucas.souza@itbam.org.br>
2021-09-15 15:14:48 -0400

    feat: saving photos to the filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
-export interface Photo {
+export interface CameraPhoto {
   filepath: string;
   webviewPath: string;
 }
@@ -12,7 +17,7 @@ export interface Photo {
   providedIn: 'root',
 })
 export class PhotoService {
-  public photos: Photo[] = [];
+  public photos: CameraPhoto[] = [];
 
   constructor() {}
 
@@ -23,9 +28,41 @@ export class PhotoService {
       quality: 100,
     });
 
-    this.photos.unshift({
-      filepath: 'soon...',
-      webviewPath: capturedPhoto.webPath,
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(cameraPhoto: Photo) {
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath,
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: Photo) {
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 }
